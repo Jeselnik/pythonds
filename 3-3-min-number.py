941639db3e9dfe454c0e3cd60b33c97f336fafb2
Edward Jeselnik <eddie@jeselnik.xyz>
2023-04-30 14:13:00 +1000

    fix quadratic min number

--- 3-3-min-number.py
+++ 3-3-min-number.py
@@ -24,20 +24,25 @@ O(n^2)
 def minNumber(n):
     min = n[0]
     for x in n:
+        smallest = True
         for i in n:
-            if x < i:
-                min = x
+            if x > i:
+                smallest = False
+        if smallest:
+            min = x 
     return min
 
 
 start = time.time()
-a = minNumberLinear(list(range(0, 100000)))
+a = minNumberLinear(list(range(0, 10000)))
+print(a)
 end = time.time()
 
 print(end-start)
 
 startB = time.time()
-b = minNumber(list(range(0, 100000)))
+b = minNumber(list(range(0, 10000)))
+print(b)
 endB = time.time()
 
 print(endB-startB)
